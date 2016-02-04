5f6655071d6b56367e425931a40b61d1db46efbb
Stacy Andrews <stacya@signiq.com>
2016-02-04 15:55:32 +0800

    fixing url patterns

--- db_file_storage/urls.py
+++ db_file_storage/urls.py
@@ -1,18 +1,18 @@
 # django
 from django.conf.urls import patterns, url
-
+from db_file_storage.views import get_file
 
 urlpatterns = patterns(
     'db_file_storage',
     url(
         r'^download/',
-        'views.get_file',
+        get_file,
         {'add_attachment_headers': True},
         name='db_file_storage.download_file'
     ),
     url(
         r'^get/',
-        'views.get_file',
+        get_file,
         {'add_attachment_headers': False},
         name='db_file_storage.get_file'
     )
